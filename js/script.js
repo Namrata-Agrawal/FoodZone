f7d4e2ce8d1b39dd10431ba9ae87166bbfc3dc34
Namrata-Agrawal <aggarwalnamrata644@gmail.com>
2022-02-10 20:16:43 +0530

    Add Menu Section

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times');
         $('.narbar').toggleClass('nav-toggle');
     });
+
+    $(window).on('scroll load', function(){
+        $('#menu-bar').removeClass('fa-times');
+        $('.narbar').removeClass('nav-toggle');
+    });
+
+    $('.menu .list .btn').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+
+        let src =$(this).attr('data-src');
+        $('#menu-img').attr('src',src);        
+    });
 });
