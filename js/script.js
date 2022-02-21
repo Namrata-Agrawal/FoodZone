99a9a478b38d301b166199bbcd81689d43005f19
Namrata-Agrawal <aggarwalnamrata644@gmail.com>
2022-02-21 11:39:24 +0530

    Add AOS Section

--- js/script.js
+++ js/script.js
@@ -8,6 +8,22 @@ $(document).ready(function(){
     $(window).on('scroll load', function(){
         $('#menu-bar').removeClass('fa-times');
         $('.narbar').removeClass('nav-toggle');
+
+        //scroll spy
+
+        $('section').each(function(){
+            let top= $(window).scrollTop();
+            let offset = $(this).offset().top - 200;
+            let height = $(this).height();
+            let id = $(this).attr('id');
+
+            if(top>offset && top<offset+ height){
+                $('.narbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        })
+
+
     });
 
     $('.menu .list .btn').click(function(){
@@ -16,4 +32,5 @@ $(document).ready(function(){
         let src =$(this).attr('data-src');
         $('#menu-img').attr('src',src);        
     });
+
 });
